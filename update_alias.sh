d0a19dc5daa26a0a30f403e0ea3cfd3b0be14912
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-01-26 14:32:17 +0900

    Update update_alias.sh

--- update_alias.sh
+++ update_alias.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-echo 123
+echo update
+
+wget -O - https://raw.githubusercontent.com/shj0000/shell-script/main/update_alias.sh >> ~/.bash_aliases
+source ~/.bash_aliases
+
+echo finish update
