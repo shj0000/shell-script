da2298bbe6779308e993087c93a0eba532f12223
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-01-27 17:09:00 +0900

    Update install_k8s_master_node.sh

--- sh/install_k8s_master_node.sh
+++ sh/install_k8s_master_node.sh
@@ -12,20 +12,22 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Load the following kernel modules on all the nodes,
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
+cat <<EOF > /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
+
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
 # Set the following Kernel parameters for Kubernetes, run beneath tee command
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+cat <<EOF > /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
-EOF 
+EOF
+
 
 # Reload the above changes, run
 sudo sysctl --system
