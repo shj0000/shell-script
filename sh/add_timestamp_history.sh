522b95a846bda076af6f4792e1fa06b86b9d9b5a
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 15:17:40 +0900

    Update add_timestamp_history.sh

--- sh/add_timestamp_history.sh
+++ sh/add_timestamp_history.sh
@@ -12,12 +12,9 @@ grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 # Append history immediately after each command
 LINE='export CUSTOM_HISTORY_FILE=/tmp/hfile_$(id -u -n)_$(date +%F).log'
-FILE='/etc/profile'
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-
-LINE='export CUSTOM_HISTORY_FILE=/tmp/hfile_$(id -u -n)_$(date +%F).log'
 FILE='/etc/bash.bashrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
 
 read -r -d '' LINE << EOM
 export PROMPT_COMMAND='history -a && echo "\$(id -u -n) $(who am i)  \$(history |tail -1)" >> \$CUSTOM_HISTORY_FILE '
