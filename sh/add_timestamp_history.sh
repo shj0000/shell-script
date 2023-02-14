7f794946d1f639ea2e9f440a0e0d9b9994dbb03f
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 15:25:42 +0900

    Update add_timestamp_history.sh

--- sh/add_timestamp_history.sh
+++ sh/add_timestamp_history.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
+FILE='/etc/bash.bashrc'
 # Change max history size
 LINE='HISTFILESIZE=99000'
-FILE='/etc/profile'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 # Add timestamp to Linux history
 LINE='export HISTTIMEFORMAT="%F %T -- "'
-FILE='/etc/profile'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
+
+
+FILE='/etc/bash.bashrc'
 # Append history immediately after each command
 LINE='export CUSTOM_HISTORY_FILE=/tmp/hfile_$(id -u -n)_$(date +%F).log'
-FILE='/etc/bash.bashrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
-
+# Write Detail Log
 read -r -d '' LINE << EOM
 export PROMPT_COMMAND='history -a && echo "\$(id -u -n) \$(who am i)  \$(history |tail -1)" >> \$CUSTOM_HISTORY_FILE '
 EOM
-
-FILE='/etc/bash.bashrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
