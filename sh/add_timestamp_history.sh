5b1cfc5341f464f03c79431fe8381be7b325df97
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 12:46:56 +0900

    Update add_timestamp_history.sh

--- sh/add_timestamp_history.sh
+++ sh/add_timestamp_history.sh
@@ -9,3 +9,8 @@ grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 LINE='export PROMPT_COMMAND="history -a"'
 FILE='/etc/profile'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# Max History Size
+LINE='HISTFILESIZE=99000'
+FILE='/etc/profile'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
