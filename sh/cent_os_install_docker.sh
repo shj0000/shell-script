0dd1f2113f28d9d02daeb0950698f4587f113b4f
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 17:42:34 +0900

    Update cent_os_install_docker.sh

--- sh/cent_os_install_docker.sh
+++ sh/cent_os_install_docker.sh
@@ -15,4 +15,4 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
                   
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
