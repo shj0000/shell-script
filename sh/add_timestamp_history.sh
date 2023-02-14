88ef9011877d77c80161897ca652b7c9c1b97ab6
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 12:44:31 +0900

    Create add_timestamp_history.sh

---
+++ sh/add_timestamp_history.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add timestamp to Linux history
+LINE='export HISTTIMEFORMAT="$USER %F %T -- "'
+FILE='/etc/profile'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# Append history immediately after each command
+LINE='export PROMPT_COMMAND="history -a"'
+FILE='/etc/profile'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
