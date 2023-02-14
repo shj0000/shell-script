3b3339e9c361ca4e69e8f358194177a4e77e08b5
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-02-14 15:13:12 +0900

    Update add_timestamp_history.sh

--- sh/add_timestamp_history.sh
+++ sh/add_timestamp_history.sh
@@ -23,5 +23,5 @@ read -r -d '' LINE << EOM
 export PROMPT_COMMAND='history -a && echo "\$(id -u -n) $(who am i)  \$(history |tail -1)" >> \$CUSTOM_HISTORY_FILE '
 EOM
 
-FILE='/etc/profile'
+FILE='/etc/bash.bashrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
