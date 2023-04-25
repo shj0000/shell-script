8912ba798005c6a4ccf8ef443317576a296130c7
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-04-25 10:30:26 +0900

    Create add_timestamp_history_centos.sh

---
+++ sh/add_timestamp_history_centos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILE='/etc/profile'
+# Change max history size
+LINE='HISTFILESIZE=99000'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# Add timestamp to Linux history
+LINE='export HISTTIMEFORMAT="%F %T -- "'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# if logic
+## cenos /etc/bashrc
+## ubuntu /etc/bash.bashrc
+FILE='/etc/bashrc'
+# Append history immediately after each command
+LINE='export CUSTOM_HISTORY_FILE=/tmp/hfile_$(id -u -n)_$(date +%F).log'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# Write Detail Log
+read -r -d '' LINE << EOM
+export PROMPT_COMMAND='history -a && echo "\$(id -u -n) \$(who am i)  \$(history |tail -1)" >> \$CUSTOM_HISTORY_FILE '
+EOM
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
