f85d21a14e54d207a0c5885ad9b19f854356ac01
shj0000 <89052067+shj0000@users.noreply.github.com>
2023-07-28 10:59:45 +0900

    Update add_timestamp_history_centos.sh

--- sh/add_timestamp_history_centos.sh
+++ sh/add_timestamp_history_centos.sh
@@ -13,8 +13,10 @@ grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 ## cenos /etc/bashrc
 ## ubuntu /etc/bash.bashrc
 FILE='/etc/bashrc'
+HISTORY_FILE_PATH=~/.history/
+mkdir -p $HISTORY_FILE_PATH
 # Append history immediately after each command
-LINE='export CUSTOM_HISTORY_FILE=/tmp/hfile_$(id -u -n)_$(date +%F).log'
+LINE='export CUSTOM_HISTORY_FILE=$(echo $HISTORY_FILE_PATH)hfile_$(id -u -n)_$(date +%F).log'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 # Write Detail Log
